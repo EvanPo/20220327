1d9e376d1508dc25346e7ed24a81e590cb73b020
EvanPo <83526644+EvanPo@users.noreply.github.com>
2023-05-11 13:03:51 +0800

    JS BS

---
+++ dist/js/js_base.js
@@ -0,0 +1,8 @@
+let age = 20;
+let firstname = 'Evan';
+let lastname = 'Po';
+let country = 'Taiwan';
+
+console.log('(' + country + ') ' + firstname + ' ' + lastname);
+
+console.log(`(${country}) ${firstname} ${lastname}`);
