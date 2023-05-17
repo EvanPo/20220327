9166fb1fa62b51eecbb999b7a64410dd2c142501
EvanPo <83526644+EvanPo@users.noreply.github.com>
2023-05-17 19:33:43 +0800

    操控＆抓取ＤＯＭ

---
+++ dist/js/js_99.js
@@ -0,0 +1,9 @@
+let main = document.querySelector('#main')
+console.log(main);
+
+main.innerHTML = 'Super man';
+//main.innerHTML = 'Super man';
+
+
+let lis = document.querySelectorAll('#menu li')
+console.log(lis);
