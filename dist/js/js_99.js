adf84cc88777a2a65176f20592008adf1d4b0fc0
EvanPo <83526644+EvanPo@users.noreply.github.com>
2023-05-17 19:42:56 +0800

    show number

--- dist/js/js_99.js
+++ dist/js/js_99.js
@@ -1,9 +1,18 @@
-let main = document.querySelector('#main')
+let main = document.querySelector('#main');
 console.log(main);
 
 main.innerHTML = 'Super man';
 //main.innerHTML = 'Super man';
 
 
-let lis = document.querySelectorAll('#menu li')
+let lis = document.querySelectorAll('#menu li');
 console.log(lis);
+
+let num1 = document.querySelector('#num-1');
+let showBtn = document.querySelector('#show-btn');
+
+num1.value = '...........'
+
+showBtn.addEventListener('click', () => {
+    num1.value = '1234567'
+});
